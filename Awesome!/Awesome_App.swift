54593a9b842ba4539c4b8c0841230e72161de671
gallaugher <john.gallaugher@bc.edu>
2026-09-14 16:40:50 -0400

    Initial Commit

---
+++ Awesome!/Awesome_App.swift
@@ -0,0 +1,17 @@
+//
+//  Awesome_App.swift
+//  Awesome!
+//
+//  Created by John Gallaugher on 9/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Awesome_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
