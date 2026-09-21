9041c92e7b4488877b25222dcfc5f47df8079d54
gallaugher <john.gallaugher@bc.edu>
2026-09-21 12:03:04 -0400

    After Week 3

--- Awesome!/ContentView.swift
+++ Awesome!/ContentView.swift
@@ -8,12 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 250, height: 250)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+                .frame(height: 40)
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome", systemImage: "button.programmable") {
+                    message = "You Are Awesome!"
+                    imageName = "sun.max"
+                }
+                
+                Button("Great", systemImage: "pointer.arrow") {
+                    message = "You Are Great!"
+                    imageName = "sparkles"
+                }
+            }
+            .buttonStyle(.glassProminent)
+            .tint(.orange)
+            .font(.title2)
         }
         .padding()
     }
